909e2f40eff307aa1859ad0ee6dd5a161882c764
isalvlace <isaacdavid797@gmail.com>
2024-08-15 16:30:50 -0300

    Esbocos de telas pos login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:treino_flutter/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,64 +9,156 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var emailController = TextEditingController(text: "");
+  var passController = TextEditingController(text: "");
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 228, 237, 241),
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 70),
-              const Icon(Icons.person, size: 150, color: Color.fromARGB(255, 26, 64, 95)),
-              const SizedBox(height: 50),
-              Container(
-                width: double.infinity,
-                height: 30,
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                alignment: Alignment.center,
-                child: const Row(
-                  children: [
-                    Expanded(flex: 2, child: Text("Informe seu email:")),
-                    Expanded(flex: 3, child: Text("Email"))
-                  ],
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 228, 237, 241),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                Image.asset(
+                  'assets/img/white_gift_box.png',
+                  height: 150,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 30,
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                alignment: Alignment.center,
-                child: const Row(
-                  children: [
-                    Expanded(flex: 2, child: Text("Informe a senha:")),
-                    Expanded(flex: 3, child: Text("Senha"))
-                  ],
+                const SizedBox(height: 20),
+                const Text(
+                  "Já possui cadastro?", 
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
-              ),
-              Expanded(child: Container()),
-              Container(
-                width: double.infinity,
-                height: 30,
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                alignment: Alignment.center,
-                color: Colors.blue,
-                child: const Text("Login"),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                height: 30,
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                alignment: Alignment.center,
-                child: const Text("Cadastrar"),
-              ),
-              const SizedBox(height: 20)
-            ],
+                const SizedBox(height: 10),
+                const Text(
+                  "Faça seu login",
+                  style: TextStyle(fontSize: 18, color: Colors.black54),
+                ),
+                const SizedBox(height: 30),
+                _buildTextField(
+                  controller: emailController,
+                  label: "Informe seu email:",
+                  hintText: "Email",
+                  icon: Icons.person,
+                ),
+                const SizedBox(height: 15),
+                _buildTextField(
+                  controller: passController,
+                  label: "Informe a senha:",
+                  hintText: "Senha",
+                  icon: Icons.lock,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+                _buildLoginButton(emailController, passController),
+                const SizedBox(height: 20),
+                _buildFooterLinks(),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required String hintText,
+    required IconData icon,
+    bool obscureText = false,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 2, 47, 83)),
+        ),
+        const SizedBox(height: 5),
+        TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            hintText: hintText,
+            prefixIcon: Icon(icon, color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton(TextEditingController emailController, TextEditingController passController) {
+  return SizedBox(
+    width: double.infinity,
+    child: ElevatedButton(
+      onPressed: () {
+        if (emailController.text.trim() == "isaacdavid797@gmail.com" && passController.text.trim() == "666") {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MainPage(),
+            ),
+          ).then((_) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Login successful!")),
+            );
+          });
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Erro ao tentar logar!")),
+          );
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        backgroundColor: Colors.blue,
+        textStyle: const TextStyle(fontSize: 16),
+      ),
+      child: const Text("Login"),
+    ),
+  );
+}
+
+  Widget _buildFooterLinks() {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            // implementação de esqueceu a senha aqui
+          },
+          child: const Text(
+            "Esqueci minha senha",
+            style: TextStyle(
+              color: Color.fromARGB(255, 140, 59, 231),
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        GestureDetector(
+          onTap: () {
+            // implementação de criar a conta aqui
+          },
+          child: const Text(
+            "Criar conta",
+            style: TextStyle(
+              color: Color.fromARGB(255, 13, 100, 16),
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.jumpToPage(index); // Navega para a página correspondente
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Olá mundo!")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("Isaac David"),
+              accountEmail: Text("isaacdavid797@email.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://www.example.com/profile_image.png',
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Início'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.search),
+              title: const Text('Buscar'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Notificações'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Configurações'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                _onItemTapped(3);
+                Navigator.pop(context);
+              },
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Ajuda'),
+              selected: _selectedIndex == 4,
+              onTap: () {
+                _onItemTapped(4);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Sair'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: const [
+          Center(child: Text('Página de Início')),
+          Center(child: Text('Página de Busca')),
+          Center(child: Text('Página de Notificações')),
+          Center(child: Text('Página de Configurações')),
+          Center(child: Text('Página de Ajuda')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue, // Cor do item selecionado
+        unselectedItemColor: Colors.grey, // Cor dos itens não selecionados
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notificações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configurações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: 'Ajuda',
+          ),
+        ],
+      ),
+    );
+  }
+}
